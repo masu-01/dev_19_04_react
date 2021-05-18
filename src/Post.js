984944bb0bb3f6889e314f7482edbb8da797cb35
Mayumi Suzuki <l0meet07@gmail.com>
2021-05-19 06:10:17 +0900

    materialUI追加した

--- src/Post.js
+++ src/Post.js
@@ -1,26 +1,48 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
 
 // Feed.jsからfirebaseのデータをPost.jsに流す（props）
-
-// ①propsを受け取る
+// ①propsを受け取る（つまり、このデータがわたってくるよ！っていうものを書いてあげる）
 const Post = ({text, img, timestamp} ) => {
+    const classes = useStyles();
+
     return (
-        <div className="result-area">
+        <>
+        <Card className={classes.root}>
+            <CardActionArea>
+                {/* 画像を表示　imgタグを使って、imgのURLをsrc={xxx}に渡してあげる */}
+                <CardMedia className={classes.media} image={img} title={text} />
+                {/* <div className="text">{text}</div> */}
 
-            {/* ②テキストが渡ってくる */}
-            <div className="text">{text}</div>
-
-            {/* ③画像を表示 */}
-            <div>
-                <img src={img} alt="" style={{ width:'200px'}}/>
-            </div>
-
-            {/* ④日付を表示 */}
-            <div className="date">
-                {new Date(timestamp?.toDate()).toLocaleString()}
-            </div>
-            
-        </div>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {/* 記述1. テキスト情報が渡ってくる */}
+                        {text}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {new Date(timestamp?.toDate()).toLocaleString()}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+        </>
     );
 };
 
