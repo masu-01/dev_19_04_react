3c1256a16b62e7282fd21060465eda2c88c4410a
Mayumi Suzuki <l0meet07@gmail.com>
2021-05-21 05:18:35 +0900

    水平スクロールがうまく行かない

--- src/Feed.js
+++ src/Feed.js
@@ -2,9 +2,43 @@ import React, { useState, useEffect } from "react";
 import { db } from "./firebase";
 import Post from "./Post";
 import Tweetinput from "./Tweetinput";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+// import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+// import tileData from './tileData'; →これが登録する中身（ここでいうPost）ってことかな。。。
+
+
+// material-uiの設定
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+      },
+      gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+      },
+      title: {
+        color: theme.palette.primary.light,
+      },
+      titleBar: {
+        background:
+          'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+      },
+    }));
+
 
 // Post.jsを読み込む
 const Feed = () => {
+    const classes = useStyles();
     // useState
     // forebaseに作成したデータを受け取るために必要な箱のこと(useState)
     // useStateを記述してfirebaseに登録されているデータの項目と同じにする（オブジェクト）
@@ -42,23 +76,43 @@ const Feed = () => {
 
     return (
         <div>
-            {/*  */}
             <Tweetinput />
             <hr />
+            <div className={classes.root}>
+                <GridList className={classes.gridList} cols={2.5}>
+                    {posts.map((tile) => (      //ここのtileDataをpostsに書き換えた
+                        <GridListTile key={tile.img}>
+                        <img src={tile.img} alt={tile.title} />
+                        <GridListTileBar
+                        title={tile.title}
+                        classes={{
+                            root: classes.titleBar,
+                            title: classes.title,
+                        }}
+                        actionIcon={
+                            <IconButton aria-label={`star ${tile.title}`}>
+                            <StarBorderIcon className={classes.title} />
+                            </IconButton>
+                        }
+                        />
+                        </GridListTile>
+                    ))}
+                </GridList>
 
-            {/* Post.jsを読み込む */}
-            {/* 記述3 postsの中にはfirebaseのデータがページが読み込まれた際に取得できています */}
-            {/* postsをes6のmapを使って１件ずつ表示します */}
-            {posts.map((postItem) => (
-            //es6のmapを使うときは「mapを使って処理をしている箇所で」[key]の指定が必要です
-            //keyがあるとバーチャルドムのkeyが指定できる？
-                <Post
-                    key={postItem.id}
-                    img={postItem.image}
-                    text={postItem.text}
-                    timestamp={postItem.timestamp}
-                />
-            ))}
+                {/* Post.jsを読み込む */}
+                {/* 記述3 postsの中にはfirebaseのデータがページが読み込まれた際に取得できています */}
+                {/* postsをes6のmapを使って１件ずつ表示します */}
+                {posts.map((postItem) => (
+                //es6のmapを使うときは「mapを使って処理をしている箇所で」[key]の指定が必要です
+                //keyがあるとバーチャルドムのkeyが指定できる？
+                    <Post
+                        key={postItem.id}
+                        img={postItem.image}
+                        text={postItem.text}
+                        timestamp={postItem.timestamp}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
--- src/Post.js
+++ src/Post.js
@@ -5,18 +5,17 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
     root: {
-      maxWidth: 345,
+      maxWidth: 400,
     },
     media: {
-      height: 140,
+      height: 240,
     },
-  });
 
+  });
 
 // Feed.jsからfirebaseのデータをPost.jsに流す（props）
 // ①propsを受け取る（つまり、このデータがわたってくるよ！っていうものを書いてあげる）
@@ -25,23 +24,26 @@ const Post = ({text, img, timestamp} ) => {
 
     return (
         <>
-        <Card className={classes.root}>
-            <CardActionArea>
-                {/* 画像を表示　imgタグを使って、imgのURLをsrc={xxx}に渡してあげる */}
-                <CardMedia className={classes.media} image={img} title={text} />
-                {/* <div className="text">{text}</div> */}
+          <Card className={classes.root}>
+            <div style={{ width: '100%' }}>
+              <CardActionArea>
+                  {/* 画像を表示　imgタグを使って、imgのURLをsrc={xxx}に渡してあげる */}
+                  <CardMedia className={classes.media} image={img} title={text} />
+                  {/* <div className="text">{text}</div> */}
 
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {/* 記述1. テキスト情報が渡ってくる */}
-                        {text}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {new Date(timestamp?.toDate()).toLocaleString()}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                  <CardContent>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                          {/* 記述1. テキスト情報が渡ってくる */}
+                          {text}
+                      </Typography>
+                      <Typography gutterBottom variant="h5" component="h2">
+                          {new Date(timestamp?.toDate()).toLocaleString()}
+                      </Typography>
+                  </CardContent>
+              </CardActionArea>
+            </div>
+          </Card>          
+
         </>
     );
 };
